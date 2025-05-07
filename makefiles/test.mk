3b6807a87b8f5577daeb2a35ac0a5bbb5dac4bb7
June Carlson <16000407+notvelleda@users.noreply.github.com>
2025-05-07 14:45:09 -0500

    makefiles: test binaries now use -O2 for optimization

--- makefiles/test.mk
+++ makefiles/test.mk
@@ -4,7 +4,7 @@ all: $(BINARY) test
 
 DEBUG_FLAG != [ "$(DEBUG)" = y ] && echo "-DDEBUG" || echo ""
 
-CFLAGS += -O0 -I$(CWD) -I$(PROJECT_ROOT)/core/include -I$(PROJECT_ROOT)/core/common -I$(PROJECT_ROOT)/test/include -DUNDER_TEST $(DEBUG_FLAG) -D_start=entry_point
+CFLAGS += -O2 -I$(CWD) -I$(PROJECT_ROOT)/core/include -I$(PROJECT_ROOT)/core/common -I$(PROJECT_ROOT)/test/include -DUNDER_TEST $(DEBUG_FLAG) -D_start=entry_point
 LDFLAGS += -L$(PROJECT_ROOT)/build/userland_low_level
 LIBFLAGS = -l$(TEST_HARNESS)
 SOURCE_FILES != find . -name "*.c" 2>/dev/null
