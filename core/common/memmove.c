542f42510e6c0eff12dc1bc6e7ba96ddacbd081f
June Carlson <16000407+notvelleda@users.noreply.github.com>
2025-06-22 17:03:47 -0500

    core/common: reverted memset, actually attributed memset & memmove now

--- core/common/memmove.c
+++ core/common/memmove.c
@@ -1,3 +1,6 @@
+// memmove from the Embedded Artistry Public Resources repository, under the CC0 license
+// https://github.com/embeddedartistry/embedded-resources/blob/9733be2eacdc3ede53b593dda9580ac21ca2283a/examples/libc/string/memmove.c
+
 #include "string.h"
 
 void *memmove(void *s1, const void *s2, size_t n) {
--- core/common/memset.c
+++ core/common/memset.c
@@ -1,5 +1,8 @@
-// MUSL memset implementation:
-// https://github.com/esmil/musl/blob/194f9cf93da8ae62491b7386edf481ea8565ae4e/src/string/memset.c or https://git.musl-libc.org/cgit/musl/tree/src/string/memset.c
+// memset from the Embedded Artistry Public Resources repository, under the CC0 license
+// (though since i'm not sure if that applies given it's derived from musl i've included the relevant musl license below)
+// https://github.com/embeddedartistry/embedded-resources/blob/9733be2eacdc3ede53b593dda9580ac21ca2283a/examples/libc/string/memset.c
+//
+// based on https://github.com/esmil/musl/blob/194f9cf93da8ae62491b7386edf481ea8565ae4e/src/string/memset.c or https://git.musl-libc.org/cgit/musl/tree/src/string/memset.c
 //
 // Copyright © 2005-2020 Rich Felker, et al.
 //
@@ -25,91 +28,61 @@
 #include "string.h"
 #include <stdint.h>
 
-void *memset(void *dest, int c, size_t n)
-{
-	unsigned char *s = dest;
-	size_t k;
+void *memset(void *dest, int c, size_t n) {
+    unsigned char *s = dest;
+    size_t k;
 
-	/* Fill head and tail with minimal branching. Each
-	 * conditional ensures that all the subsequently used
-	 * offsets are well-defined and in the dest region. */
+    /* Fill head and tail with minimal branching. Each
+     * conditional ensures that all the subsequently used
+     * offsets are well-defined and in the dest region. */
 
-	if (!n) return dest;
-	s[0] = c;
-	s[n-1] = c;
-	if (n <= 2) return dest;
-	s[1] = c;
-	s[2] = c;
-	s[n-2] = c;
-	s[n-3] = c;
-	if (n <= 6) return dest;
-	s[3] = c;
-	s[n-4] = c;
-	if (n <= 8) return dest;
+    if (!n) {
+        return dest;
+    }
 
-	/* Advance pointer to align it at a 4-byte boundary,
-	 * and truncate n to a multiple of 4. The previous code
-	 * already took care of any head/tail that get cut off
-	 * by the alignment. */
+    s[0] = (unsigned char) c;
+    s[n - 1] = (unsigned char) c;
 
-	k = -(uintptr_t)s & 3;
-	s += k;
-	n -= k;
-	n &= (size_t) -4;
+    if (n <= 2) {
+        return dest;
+    }
 
-//#ifdef __GNUC__
-#if 0 // TODO: investigate why this doesn't work
-	typedef uint32_t __attribute__((__may_alias__)) u32;
-	typedef uint64_t __attribute__((__may_alias__)) u64;
+    s[1] = (unsigned char) c;
+    s[2] = (unsigned char) c;
+    s[n - 2] = (unsigned char) c;
+    s[n - 3] = (unsigned char) c;
 
-	u32 c32 = ((u32)-1)/255 * (unsigned char)c;
+    if (n <= 6) {
+        return dest;
+    }
 
-	/* In preparation to copy 32 bytes at a time, aligned on
-	 * an 8-byte bounary, fill head/tail up to 28 bytes each.
-	 * As in the initial byte-based head/tail fill, each
-	 * conditional below ensures that the subsequent offsets
-	 * are valid (e.g. !(n<=24) implies n>=28). */
+    s[3] = (unsigned char) c;
+    s[n - 4] = (unsigned char) c;
 
-	*(u32 *)(s+0) = c32;
-	*(u32 *)(s+n-4) = c32;
-	if (n <= 8) return dest;
-	*(u32 *)(s+4) = c32;
-	*(u32 *)(s+8) = c32;
-	*(u32 *)(s+n-12) = c32;
-	*(u32 *)(s+n-8) = c32;
-	if (n <= 24) return dest;
-	*(u32 *)(s+12) = c32;
-	*(u32 *)(s+16) = c32;
-	*(u32 *)(s+20) = c32;
-	*(u32 *)(s+24) = c32;
-	*(u32 *)(s+n-28) = c32;
-	*(u32 *)(s+n-24) = c32;
-	*(u32 *)(s+n-20) = c32;
-	*(u32 *)(s+n-16) = c32;
+    if (n <= 8) {
+        return dest;
+    }
 
-	/* Align to a multiple of 8 so we can fill 64 bits at a time,
-	 * and avoid writing the same bytes twice as much as is
-	 * practical without introducing additional branching. */
+    /* Advance pointer to align it at a 4-byte boundary,
+     * and truncate n to a multiple of 4. The previous code
+     * already took care of any head/tail that get cut off
+     * by the alignment. */
 
-	k = 24 + ((uintptr_t)s & 4);
-	s += k;
-	n -= k;
+    k = -(uintptr_t) s & 3;
+    s += k;
+    n -= k;
+    n &= (size_t) -4;
+    n /= 4;
 
-	/* If this loop is reached, 28 tail bytes have already been
-	 * filled, so any remainder when n drops below 32 can be
-	 * safely ignored. */
+    // Cast to void first to prevent alignment warning
+    uint32_t* ws = (uint32_t *) (void *) s;
+    uint32_t wc = c & 0xFF;
+    wc |= ((wc << 8) | (wc << 16) | (wc << 24));
 
-	u64 c64 = c32 | ((u64)c32 << 32);
-	for (; n >= 32; n-=32, s+=32) {
-		*(u64 *)(s+0) = c64;
-		*(u64 *)(s+8) = c64;
-		*(u64 *)(s+16) = c64;
-		*(u64 *)(s+24) = c64;
-	}
-#else
-	/* Pure C fallback with no aliasing violations. */
-	for (; n; n--, s++) *s = c;
-#endif
+    /* Pure C fallback with no aliasing violations. */
+    for(; n; n--, ws++) {
+        *ws = wc;
+    }
 
-	return dest;
+    return dest;
 }
